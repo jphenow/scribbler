0e80e0602051f56b55defa934761e4ee20075722
Jon Phenow <j.phenow@gmail.com>
2012-06-12 10:44:41 -0500

    Does some cleanup of Configurator per the tests, which are also included

--- lib/scribbler/configurator.rb
+++ lib/scribbler/configurator.rb
@@ -5,20 +5,30 @@ module Scribbler
     end
 
     def self.yaml_config
-      (@yaml_config ||= YAML::load(File.open(Configurator.yaml_config_path)) || {}).with_indifferent_access
+      unless @yaml_config.present?
+        if yaml_config_path.present? and File.exists?(yaml_config_path)
+          @yaml_config = YAML::load(File.open(yaml_config_path))
+        else
+          @yaml_config = {}
+        end
+      end
+      @yaml_config.with_indifferent_access
     end
 
     def self.logs
-      (@logs ||= (Configurator.yaml_config[:logs] || {})).with_indifferent_access
+      (@logs ||= (yaml_config[:logs] || {})).with_indifferent_access
     end
 
     def self.application_include
-      (@application_include ||= (Configurator.yaml_config[:application_include] || false)).with_indifferent_acces
+      if @application_include.nil?
+        @application_include = yaml_config[:application_include] || false
+      end
+      @application_include
     end
 
     def self.yaml_config_path
       begin
-        @yaml_config_path || Rails.root.join('config', 'scribbler.yml')
+        @yaml_config_path || ::Rails.root.join('config', 'scribbler.yml')
       rescue NameError
         nil
       end
