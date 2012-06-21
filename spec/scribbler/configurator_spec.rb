06beb14ff682a00df59fbe0ad2e731cd26e6f6d6
Jon Phenow <j.phenow@gmail.com>
2012-06-20 23:55:26 -0500

    test log directory setting

--- spec/scribbler/configurator_spec.rb
+++ spec/scribbler/configurator_spec.rb
@@ -21,5 +21,34 @@ module Scribbler
         subject.application_include.should == true
       end
     end
+
+    describe "log directory" do
+      it "tries a rails root when Rails defined" do
+        root_stub = stub
+        root_stub.should_receive(:join).with 'log'
+        Rails = stub(:root => root_stub)
+        subject.log_directory
+      end
+
+      it "falls back to pwd/log without rails" do
+        dir = "dir/"
+        Rails.should_receive(:root).and_raise(NameError)
+        Dir.should_receive(:pwd).and_return('dir')
+        subject.log_directory.should == 'dir/log'
+      end
+
+      it "sets the log directory" do
+        var_log = File.new "/var/log"
+        subject.log_directory = var_log
+        subject.log_directory.should == var_log
+        subject.log_directory = nil
+
+        # Check the reset
+        Rails.should_receive(:root).and_raise(NameError)
+        Dir.should_receive(:pwd).and_return('dir')
+        subject.log_directory.should == 'dir/log'
+        subject.log_directory
+      end
+    end
   end
 end
