d0062ac18f667cec41935534a9cca16704f205e9
Jon Phenow <j.phenow@gmail.com>
2013-02-27 21:55:37 -0600

    objectify me

--- lib/scribbler/logger.rb
+++ lib/scribbler/logger.rb
@@ -1,45 +1,57 @@
 module Scribbler
   class Logger
-  # Public: Save ourselves some repetition. Notifies error to NewRelic
-  # and drops given string into a given log.
-  #
-  # location  - Either a pathname from the above method or symbol for an above
-  #             method
-  # options   - Hash of options for logging on Ngin
-  #           :error            - Error object, mostly for passing to NewRelic
-  #           :message          - Message to log in the actual file
-  #           :custom_fields    - Custom fields dropped into the default template
-  #           :template         - Whether or not to use the template at this log
-  #           :new_relic        - Notify NewRelic of the error (default: true)
-  #
-  # Examples
-  #
-  #   log(Ngin.subseason_log_location, :error => e, :message  => "Error message stuff", :new_relic => false)
-  #
-  #   log(:subseason, :error => e, :message => "Error message stuff")
-  #
-  #   log(:subseason, :message => "Logging like a bauss")
-  #
-  # Returns Nothing.
-    def log(location, options = {})
-      options = {
-        :template => config.use_template_by_default,
-        :stack_frame => options[:call_stack] ? Kernel.caller[1..-1] : nil
-      }.merge options
-      begin
-        NewRelic::Agent.notice_error(options[:error]) if options[:error] and options[:new_relic] != false
-      rescue NameError
-        nil
-      end
+    private
+    attr_accessor :location
+    attr_accessor :options
 
-      apply_to_log location, options
+    public
+    # Public: Save ourselves some repetition. Notifies error to NewRelic
+    # and drops given string into a given log.
+    #
+    # location  - Either a pathname from the above method or symbol for an above
+    #             method
+    # options   - Hash of options for logging on Ngin
+    #           :error            - Error object, mostly for passing to NewRelic
+    #           :message          - Message to log in the actual file
+    #           :custom_fields    - Custom fields dropped into the default template
+    #           :template         - Whether or not to use the template at this log
+    #           :new_relic        - Notify NewRelic of the error (default: true)
+    #
+    # Examples
+    #
+    #   log(Ngin.subseason_log_location, :error => e, :message  => "Error message stuff", :new_relic => false)
+    #
+    #   log(:subseason, :error => e, :message => "Error message stuff")
+    #
+    #   log(:subseason, :message => "Logging like a bauss")
+    #
+    # Returns Nothing.
+    def log(location, options = {})
+      self.location = location
+      self.options = gather_log_options options
+      notify_new_relic options[:error], options[:new_relic]
+
+      apply_to_log
     end
 
-    def log_at(file_name)
-      LogLocation.new.find_path file_name
+    def actual_log_location
+      LogLocation.new.find_path location
     end
 
     private
+
+    def notify_new_relic(error, new_relic)
+      NewRelic::Agent.notice_error(error) if error and new_relic != false
+    rescue NameError
+      nil
+    end
+
+    def gather_log_options(given_options)
+      {
+        :template => config.use_template_by_default,
+        :stack_frame => given_options[:call_stack] ? Kernel.caller[1..-1] : nil
+      }.merge given_options
+    end
 
     def config
       Scribbler.config
@@ -69,7 +81,7 @@ module Scribbler
     #     EXAMPLE
     #
     # Returns nil, a string, or a string built with calling the Configurator.template method
-    def build_with_template(options={})
+    def build_with_template
       if options[:message].present?
         options[:message] = options[:message].strip_heredoc.rstrip
         options[:template] ? config.template.call(options) : options[:message]
@@ -86,20 +98,20 @@ module Scribbler
     #
     # Examples
     #
-    #   Base.apply_to_log :some_loc, :message => "...", :template => false, :error => e
+    #   apply_to_log  # WITH :some_loc, :message => "...", :template => false, :error => e
     #   # => Nothing
     #
     # Returns Nothing
-    def apply_to_log(location, options={})
+    def apply_to_log
       if can_apply_to_log? location, options
-        File.open log_at(location), 'a' do |f|
-          f.puts build_with_template(options)
+        File.open actual_log_location, 'a' do |f|
+          f.puts build_with_template
         end
       end
     end
 
     # TODO: Fix to work with any template
-    def can_apply_to_log?(location, options)
+    def can_apply_to_log?
       location.present? and
         (options[:message].present? or
          options[:object].present? or
