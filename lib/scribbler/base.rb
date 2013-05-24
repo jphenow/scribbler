de0cd9869067b63478fd0eb8e93838e9f142d300
Jon Phenow <j.phenow@gmail.com>
2013-05-24 11:09:34 -0500

    provide deprecated configure on scribbler/base

--- lib/scribbler/base.rb
+++ lib/scribbler/base.rb
@@ -1,14 +1,23 @@
-module Scribbler::Base
-  def self.method_missing(method, *args, &block)
-    if Scribbler.respond_to? method
-      puts "DEPRECATION WARNING: Scribbler is now preferred over Scribbler::Base see github for more info"
-      Scribbler.public_send method, *args, &block
-    else
-      super
+module Scribbler
+  module Base
+    def self.configure(&block)
+      ActiveSupport::Deprecation.deprecated_method_warning :configure,
+        "Scribbler is now preferred over Scribbler::Base see github for more info"
+      Scribbler.configure { |config| config.instance_eval(&block) }
     end
-  end
 
-  def self.respond_to?(method, include_private = false)
-    Scribbler.respond_to?(method) || super
+    def self.method_missing(method, *args, &block)
+      if Scribbler.respond_to? method
+        ActiveSupport::Deprecation.deprecated_method_warning method,
+          "Scribbler is now preferred over Scribbler::Base see github for more info"
+        Scribbler.public_send method, *args, &block
+      else
+        super
+      end
+    end
+
+    def self.respond_to?(method, include_private = false)
+      Scribbler.respond_to?(method) || super
+    end
   end
 end
